a7dfda247c34b6db7136617d8a6f3c1aff09249e
bmiftah <miftahbedru@gmail.com>
2022-05-17 17:43:57 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -58,19 +58,35 @@ def Generator(X_list, y_list, batch_size = 16):
 # Data Augmentation method
 def augmentation(imgs, masks): 
     for img, mask in zip(imgs, masks):
-        img = cv2.imread("foetal-head-us/train/" + img)
-        mask = cv2.imread("foetal-head-us/train/" + mask)
-        train_img_aug.append(img)
+        img = cv2.imread("abdomen_train//" + img)
         
+        mask_name, mask_extension = os.path.splitext(str(mask))
+        #print(mask_name,"   ",mask_extension)
+        #exit()
+        mask = cv2.imread('abdomen_mask/'+mask)
+        #print(img.shape)
+        #print(mask.shape)
+        # print(mask)
+        #filename, file_extension = os.path.splitext('/path/to/somefile.ext')
+      
+        
+        train_img_aug.append(img)
         train_mask_aug.append(mask)
+
+        # flip the image/mask to right 
         img_lr = np.fliplr(img)
         mask_lr = np.fliplr(mask)
+        # flip the image/mask up side down 
         img_up = np.flipud(img)
         mask_up = np.flipud(mask)
+        # flip the image/mask up side down on earlier flipped imgs/masks
         img_lr_up = np.flipud(img_lr)
         mask_lr_up = np.flipud(mask_lr)
+        # flip the image/mask --- on earlier flipped data ..
         img_up_lr = np.fliplr(img_up)
         mask_up_lr = np.fliplr(mask_up)
+
+
         train_img_aug.append(img_lr)
         train_mask_aug.append(mask_lr)
         train_img_aug.append(img_up)
@@ -82,13 +98,24 @@ def augmentation(imgs, masks):
         
 if __name__ == "__main__":
     # training data path
-    path_train = config.PATH_TRAIN
-    file_list_train = sorted(os.listdir(path_train))
+    path_train_image = config.PATH_TRAIN_IMAGE
+    path_train_mask = config.PATH_TRAIN_MASK
+
+    file_list_train_image = sorted(os.listdir(path_train_image))
+    file_list_train_mask = sorted(os.listdir(path_train_mask))
     
-    # test data path
+    print("train image files list ",file_list_train_image)
+    print("train mask files list ",file_list_train_mask)
+
+    
+    # separte image n mask and save them in disk
+
+    
+    # test-data path
     path_test = config.PATH_TEST
     file_list_test = sorted(os.listdir(path_test))
-    
+    print(" test files list ",file_list_test)
+    #exit()
     # target image size
     IMG_HEIGHT = config.IMG_HEIGHT
     IMG_WIDTH = config.IMG_WIDTH
@@ -96,34 +123,75 @@ if __name__ == "__main__":
     smooth = config.smooth
     train_image = []
     train_mask = []
-    for idx, item in enumerate(file_list_train):
-        if idx % 2 == 0:
-            train_image.append(item)
-        else:
-            train_mask.append(item)
+    print(" Separtating image and mask ...Mask need to be saved in a separate directory ")
+    #for idx, item in enumerate(file_list_train):
+    for img, mask in zip(file_list_train_image,file_list_train_mask):
+        #if idx % 2 == 0:
+        train_image.append(img)
+            # enable this line to fetch image - not mask
+            #image = cv2.imread('train/'+item)
+            # separate image name from its extension
+            #image_name, image_extension = os.path.splitext(str(item))
+            # print(image.shape)
+            # print(image_name)
+            # print(image_extension)
+            # ENABLE THIS LINE if image is not saved in separate directory  and comment it 
+            #cv2.imwrite('mask1_image/'+str(image_name) +str(image_extension),image )
+        #else:
+            #  enable this line to fetch mask - not image
+            #mask = cv2.imread('train/'+item)
+            #mask_name, mask_extension = os.path.splitext(str(item))
+            #print(msk.shape)
+            #print(mask_name)
+            #print(mask_extension)
+            # ENABLE THIS LINE if mask is not saved in separate directory  and comment it 
+            #cv2.imwrite('mask/'+str(mask_name) +str(mask_extension),mask )
+            
+            #cv2.imwrite('item)
+            #Image.SAVE(item)
+            # print(item)
+            # mask_name, mask_extension = os.path.splitext(str(item))
+            # print(mask_name,"   ",mask_extension)
+            # item.save("mask/"+mask_name+'.png')
+        train_mask.append(mask)
+    #exit()  - if u are saving image and mask separately - better uncomment it  and later comment it
     print("Number of US training images is {}".format(len(train_image)))
     print("Number of US training masks is {}".format(len(train_mask))) 
     
     ## Storing data
     X = []
     y = []
+    # print(" image and mask ")
+    
+    # print(train_image)
+    # print(train_mask)
+    
     for image, mask in zip(train_image, train_mask):
-        X.append(np.array(Image.open(path_train+image)))
-        y.append(np.array(Image.open(path_train+mask)))
+        X.append(np.array(Image.open(path_train_image+image)))
+        y.append(np.array(Image.open(path_train_mask+mask)))
 
-    X = np.array(X)
-    y = np.array(y)
+    #X = np.array(X)
+    #y = np.array(y)
     # data augmentation
     train_img_aug = []
     train_mask_aug = []
-    augmentation(train_image, train_mask)
     
-    #split training data
+    augmentation(train_image, train_mask)
+    print(" Agumentation of data done ! ")
+    # print(len(train_image))
+    # print(len(train_img_aug))
+    # print(train_image.shape)
+    # print(train_img_aug.shape)
+
+    #exit()
+    #split training data into trainign and validation
+    print(" Number of training data after agumentation",len(train_img_aug))
+    
     X_train, X_val, y_train, y_val = train_test_split(train_img_aug, train_mask_aug, test_size = 0.2, random_state = 1)
 
     # set training parameters
-    epochs = 50
-    batch_size = 16
+    epochs = 15
+    batch_size = 8
     steps_per_epoch = int(len(X_train) / batch_size)
     validation_steps = int(len(X_val) / batch_size)
 
@@ -137,7 +205,9 @@ if __name__ == "__main__":
     # Define callbacks to save model with best val_dice_coef
     checkpointer = ModelCheckpoint(filepath = config.best_model_path, monitor='val_dice_coef', verbose=1, save_best_only=True, mode='max')
     model = Model(inputs=[inputs], outputs=[output_layer])
-    model.compile(optimizer=Adam(lr = 3e-5), loss=dice_coef_loss, metrics=[dice_coef])
+    print(model)
+    #exit()
+    model.compile(optimizer=Adam(learning_rate = 3e-5), loss=dice_coef_loss, metrics=[dice_coef])
     
     results = model.fit_generator(train_gen, steps_per_epoch=steps_per_epoch, epochs = epochs,
                              validation_data = val_gen, validation_steps = validation_steps,callbacks=[checkpointer])
